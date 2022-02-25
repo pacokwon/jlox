5f630791d4bc4c63cd17b8f4ebf698d707e511c2
pacokwon <haechank@gmail.com>
2022-02-25 23:26:50 +0900

    Implement until AstPrinter

--- src/main/lox/Main.java
+++ src/main/lox/Main.java
@@ -2,6 +2,14 @@ package lox;
 
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello World!");
+    Expr expr = new Expr.Binary(
+      new Expr.Unary(
+        new Token(TokenType.MINUS, "-", null, 1),
+        new Expr.Literal(123)
+      ),
+      new Token(TokenType.STAR, "*", null, 1),
+      new Expr.Grouping(new Expr.Literal(45.67))
+    );
+    System.out.println(new AstPrinter().print(expr));
   }
 }
